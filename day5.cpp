e8cd631d0ffea141daea72c65ab07eb5a73dc633
Daniel Kramer <dkramer@lucidsci.com>
2024-12-05 17:17:09 +0000

    day 5 part 1 solution

---
+++ day5.cpp
@@ -0,0 +1,104 @@
+
+#include <vector>
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+#include <sstream>
+#include <limits>
+
+using row = std::vector<int>;
+
+struct Inp 
+{
+    std::vector<std::pair<size_t, size_t>> comp;
+    std::vector<row> order;
+};
+
+row split(const std::string& inp, const char delim) {
+    std::string tmp;
+    row r;
+    std::istringstream s(inp);
+    while (std::getline(s, tmp, delim)) {
+        r.push_back(std::stoi(tmp));
+    }
+    return r;
+}
+
+Inp getInput(const std::string& fp){
+    std::ifstream file (fp);
+    
+    Inp inp;
+
+    int mode = 0;
+    size_t bi;
+    while(file){
+     std::string tmp;
+     std::getline(file, tmp);
+        if (tmp.empty()) {
+            mode++;
+            continue;
+        }
+        switch (mode)
+        {
+        case 0:
+            bi = tmp.find_first_of('|');
+            
+            inp.comp.emplace_back(std::stoi(tmp.substr(0, bi)), std::stoi(tmp.substr(bi+1)));
+            break;
+        case 1:
+            inp.order.emplace_back(split(tmp, ','));
+            break;
+        default:
+            break;
+        }
+
+      }
+      return inp;
+}
+
+int main(const int argc, char *argv[]) {
+    if (argc != 2) {
+        std::cerr << "Invalid args" << std::endl;
+        return 1;
+    }
+
+    const auto inp = getInput(argv[1]);
+
+    // for(const auto& [f,l]: inp.comp){
+    //     std::cout << f << '|' << l << std::endl;
+    // }
+
+    // for(const auto& r: inp.order){
+    //     for(const auto& ele: r){
+    //         std::cout << ele << ',';
+    //     }
+    //     std::cout << std::endl;
+    // }
+
+    
+    int sum = 0;
+    for(const auto& r: inp.order){
+        std::unordered_map<int, int> poses;
+        for(size_t i = 0; i < r.size(); i++){
+            poses[r[i]] = i;
+        }
+        const auto middle = r[r.size()/2];
+
+        bool good = true;
+        for(const auto& [f,l]: inp.comp){
+            if((poses.contains(f) && poses.contains(l)) && poses[f] > poses[l]){
+                // std::cout << f << '|' << l << std::endl;
+                good = false;
+                break;
+            }
+        }
+        if(!good){
+            continue;
+        }
+        sum += middle;
+    }
+    std::cout << sum << std::endl;
+    return 0;
+}
