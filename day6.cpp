06ea0f94874e05d6560c2591032abaec5ed73173
Daniel Kramer <drkspace@gmail.com>
2024-12-07 00:02:45 +0000

    Redid part 1

--- day6.cpp
+++ day6.cpp
@@ -1,3 +1,4 @@
+#include <utility>
 #include <vector>
 #include <string>
 #include <fstream>
@@ -53,6 +54,15 @@ using BoxLocations = std::unordered_set<pos, hash_pair>;
 constexpr char BOX = '#';
 constexpr char GUARD = '^';
 
+[[nodiscard]] int sign(const int v) {
+    if (v>0) {
+        return 1;
+    }else if (v < 0) {
+        return -1;
+    }
+    return 0;
+}
+
 class Direction{
     public:
     enum D{
@@ -84,79 +94,90 @@ class Direction{
 class GuardState{
     using BoxHash = std::unordered_map<int, std::vector<int>>;
     public:
-        GuardState(pos g_pos, BoxLocations bl): init_pos(g_pos), box_locations(bl) {
+        GuardState(pos g_pos, BoxLocations bl, int grid_size_x, int grid_size_y): init_pos(g_pos), box_locations(std::move(bl)), grid_size_x(grid_size_x), grid_size_y(grid_size_y), t({}) {
             for(const auto& [x, y]: box_locations){
                 row_locations[x].push_back(y);
                 col_locations[y].push_back(x);
             }
-            for(auto [key, vec]: row_locations){
-                std::sort(vec.begin(), vec.end());
+            for(auto& [key, vec]: row_locations){
+                std::ranges::sort(vec);
             }
-            for(auto [key, vec]: col_locations){
-                std::sort(vec.begin(), vec.end());
+            for(auto& [key, vec]: col_locations){
+                std::ranges::sort(vec);
             }
         }
         int run(){
 
             Direction::D d = Direction::D::NORTH;
             auto cur_pos = init_pos;
-            int count = 0;
+            int count = 1; // Include the guard's starting position
             while(true){
+
                 switch (d)
                 {
                 case Direction::D::NORTH:
                     if(col_locations.contains(cur_pos.second)){
-                        auto lb = std::lower_bound(col_locations[cur_pos.second].begin(), col_locations[cur_pos.second].end(), cur_pos.first);
+                        auto lb = std::ranges::lower_bound(col_locations[cur_pos.second], cur_pos.first);
                         auto next = std::distance(col_locations[cur_pos.second].begin(), lb) - 1;
                         if(next < 0){
-                            return count;
+                            visit_mult(cur_pos, std::make_pair(-1, cur_pos.second));
+                            return t.size();
                         }
-                        count += std::abs(cur_pos.first - col_locations[cur_pos.second][next])-1;
-                        cur_pos = std::make_pair(col_locations[cur_pos.second][next]+1, cur_pos.second);
+                        count += std::abs(cur_pos.first - col_locations[cur_pos.second].at(next))-1;
+                        auto tmp_pos = std::make_pair(col_locations[cur_pos.second][next]+1, cur_pos.second);
+                        visit_mult(cur_pos, tmp_pos);
+                        cur_pos = tmp_pos;
                     }else{
-                        return count;
+                        return t.size() + cur_pos.first;
                     }
                     break;
                 case Direction::D::EAST:
                     if(row_locations.contains(cur_pos.first)){
-                        auto lb = std::upper_bound(row_locations[cur_pos.first].begin(), row_locations[cur_pos.first].end(), cur_pos.second);
-                        auto next = std::distance(row_locations[cur_pos.first].begin(), lb) - 1;
-                        if(next < 0){
-                            return count;
+                        auto ub = std::ranges::upper_bound(row_locations[cur_pos.first], cur_pos.second);
+                        auto next = std::distance(row_locations[cur_pos.first].begin(), ub);
+                        if(next >= static_cast<int>(row_locations[cur_pos.first].size())){
+                            visit_mult(cur_pos, std::make_pair(cur_pos.first, grid_size_y));
+                            return t.size();
                         }
-                        count += std::abs(cur_pos.first - row_locations[cur_pos.first][next])-1;
-                        cur_pos = std::make_pair(cur_pos.first, row_locations[cur_pos.first][next]-1);
+                        count += std::abs(cur_pos.second - row_locations[cur_pos.first].at(next))-1;
+                        auto tmp_pos = std::make_pair(cur_pos.first, row_locations[cur_pos.first][next]-1);
+                        visit_mult(cur_pos, tmp_pos);
+                        cur_pos = tmp_pos;
                     }else{
-                        return count;
+                        return t.size() + (grid_size_y-cur_pos.second);
                     }
                     break;
                 case Direction::D::SOUTH:
                     if(col_locations.contains(cur_pos.second)){
-                        auto lb = std::upper_bound(col_locations[cur_pos.second].begin(), col_locations[cur_pos.second].end(), cur_pos.first);
-                        auto next = std::distance(col_locations[cur_pos.second].begin(), lb) - 1;
-                        if(next < 0){
-                            return count;
+                        auto ub = std::ranges::upper_bound(col_locations[cur_pos.second], cur_pos.first);
+                        auto next = std::distance(col_locations[cur_pos.second].begin(), ub);
+                        if(next >= static_cast<int>(col_locations[cur_pos.second].size())){
+                            visit_mult(cur_pos, std::make_pair(grid_size_x, cur_pos.second));
+                            return t.size();
                         }
-                        count += std::abs(cur_pos.first - col_locations[cur_pos.second][next])-1;
-                        cur_pos = std::make_pair(col_locations[cur_pos.second][next]-1, cur_pos.second);
+                        count += std::abs(cur_pos.first - col_locations[cur_pos.second].at(next))-1;
+                        auto tmp_pos = std::make_pair(col_locations[cur_pos.second][next]-1, cur_pos.second);
+                        visit_mult(cur_pos, tmp_pos);
+                        cur_pos = tmp_pos;
                     }else{
-                        return count;
+                        return t.size() + (grid_size_x - cur_pos.first);
                     }
                     break;
                 case Direction::D::WEST:
                     if(row_locations.contains(cur_pos.first)){
-                        auto lb = std::lower_bound(row_locations[cur_pos.first].begin(), row_locations[cur_pos.first].end(), cur_pos.second);
+                        auto lb = std::ranges::lower_bound(row_locations[cur_pos.first], cur_pos.second);
                         auto next = std::distance(row_locations[cur_pos.first].begin(), lb) - 1;
                         if(next < 0){
-                            return count;
+                            visit_mult(cur_pos, std::make_pair(cur_pos.first, -1));
+                            return t.size();
                         }
-                        count += std::abs(cur_pos.first - row_locations[cur_pos.first][next])-1;
-                        cur_pos = std::make_pair(cur_pos.first, row_locations[cur_pos.first][next]+1);
+                        count += std::abs(cur_pos.second - row_locations[cur_pos.first].at(next))-1;
+                        auto tmp_pos = std::make_pair(cur_pos.first, row_locations[cur_pos.first][next]+1);
+                        visit_mult(cur_pos, tmp_pos);
+                        cur_pos = tmp_pos;
                     }else{
-                        return count;
+                        return t.size() + cur_pos.second;
                     }
-                    break;
-                default:
                     break;
                 }
 
@@ -169,6 +190,24 @@ class GuardState{
     const BoxLocations box_locations;
     BoxHash row_locations;
     BoxHash col_locations;
+    const int grid_size_x;
+    const int grid_size_y;
+
+    tracker t;
+
+    void visit_mult(const pos& pos1, const pos& pos2) {
+        if (pos1.first != pos2.first) {
+            for (int i = pos1.first; i != pos2.first; i+= sign(pos2.first-pos1.first)) {
+                t.emplace(i, pos1.second);
+            }
+        }
+        else {
+            for (int i = pos1.second; i != pos2.second; i+= sign(pos2.second-pos1.second)) {
+                t.emplace(pos1.first, i);
+            }
+        }
+    };
+
 };
 
 Map getInput(const std::string& fp){
@@ -178,7 +217,7 @@ Map getInput(const std::string& fp){
      std::string tmp;
      std::getline(file, tmp);
         if (!tmp.empty()) {
-            out.push_back(std::vector<char>(tmp.begin(), tmp.end()));
+            out.emplace_back(tmp.begin(), tmp.end());
         }
       }
       return out;
@@ -206,9 +245,25 @@ int main(const int argc, char *argv[]) {
         std::cout << std::endl;
     }
     // std::cout << g_pos.first <<',' << g_pos.second << std::endl;
-    GuardState s(g_pos, bl);
+    GuardState s(g_pos, bl, inp.size(), inp[0].size());
     auto c = s.run();
     std::cout << c << std::endl;
+
+    // for(const auto& [i, row]: std::ranges::views::enumerate(inp)){
+    //     for(const auto& [j, c]: std::ranges::views::enumerate(row)){
+    //         if (c == GUARD || c == BOX) {
+    //             std::cout << c;
+    //         }
+    //         else if (s.t.contains(std::make_pair(i, j))) {
+    //             std::cout << "+";
+    //         }
+    //         else{
+    //             std::cout << c;
+    //         }
+    //
+    //     }
+    //     std::cout << std::endl;
+    // }
 
     //6,3
     //7,6
