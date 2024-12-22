a8414b657446bcb00edc88382ceb518260576e8c
Daniel Kramer <drkspace@gmail.com>
2024-12-21 21:41:28 -0500

    day 21 part 2 solution

--- day21.cpp
+++ day21.cpp
@@ -9,10 +9,10 @@
 #include <cinttypes>
 #include "utils/utils.h"
 #include <optional>
-#include <pstl/execution_defs.h>
-using dt = intmax_t;
 
-constexpr dt INF = std::numeric_limits<dt>::max()/2;
+using dt = uintmax_t;
+
+constexpr dt INF = std::numeric_limits<dt>::max();
 constexpr char U='^';
 constexpr char D='v';
 constexpr char L='<';
@@ -169,6 +169,30 @@ void simdir(const std::string& seq) {
     std::cout << std::endl;
 }
 
+dt dochunk(const char c_start, const int level, const std::unordered_map<std::pair<char, char>, std::vector<std::string>, hash_tuple>& DIR_PATHS, std::unordered_map<std::tuple<char, char, int>, dt, hash_tuple>& cache, char start = A) {
+    if (cache.contains({c_start,start, level})) {
+        return cache[{c_start, start, level}];
+    }
+    // std::cout << seq << std::endl;
+    dt sum = 0;
+    // auto best = INF;
+    char curpos = start;
+
+    std::string seq2 = DIR_PATHS.find({curpos, c_start})->second[0]+'A';
+
+    if (level==25) {
+        return static_cast<dt>(seq2.size());
+    }
+    curpos = A;
+    for (const auto& c: seq2) {
+        sum += dochunk(c, level+1, DIR_PATHS, cache, curpos);
+        curpos = c;
+    }
+
+    cache[{c_start, start, level}] = sum;
+    return sum;
+}
+
 int main(const int argc, char *argv[]) {
     easyTimer<std::chrono::milliseconds> _("main ");
 
@@ -178,40 +202,40 @@ int main(const int argc, char *argv[]) {
         return 1;
     }
 
-    std::unordered_map<std::pair<char, char>, std::string, hash_tuple> DIR_PATHS;
-    DIR_PATHS[{A, U}] = "<";
-    DIR_PATHS[{U, A}] = ">";
-    DIR_PATHS[{A, R}] = "v";
-    DIR_PATHS[{R, A}] = "^";
-    DIR_PATHS[{A, D}] = "<v";
-    DIR_PATHS[{D, A}] = "^>";
-    DIR_PATHS[{A, L}] = "v<<";
-    DIR_PATHS[{L, A}] = "^>>";
+    std::unordered_map<std::pair<char, char>, std::vector<std::string>, hash_tuple> DIR_PATHS;
+    DIR_PATHS[{A, U}] = {"<"};
+    DIR_PATHS[{U, A}] = {">"};
+    DIR_PATHS[{A, R}] = {"v"};
+    DIR_PATHS[{R, A}] = {"^"};
+    DIR_PATHS[{A, D}] = {"<v"};
+    DIR_PATHS[{D, A}] = {"^>"};
+    DIR_PATHS[{A, L}] = {"v<<"};
+    DIR_PATHS[{L, A}] = {">>^"};
 
-    DIR_PATHS[{U, D}] = "v";
-    DIR_PATHS[{D, U}] = "^";
-    DIR_PATHS[{U, L}] = "<v";
-    DIR_PATHS[{L, U}] = "^>";
-    DIR_PATHS[{U, R}] = "v>";
-    DIR_PATHS[{R, U}] = "<^";
+    DIR_PATHS[{U, D}] = {"v"};
+    DIR_PATHS[{D, U}] = {"^"};
+    DIR_PATHS[{U, L}] = {"v<"};
+    DIR_PATHS[{L, U}] = {">^"};
+    DIR_PATHS[{U, R}] = {"v>"};
+    DIR_PATHS[{R, U}] = {"<^"};
 
-    DIR_PATHS[{D, L}] = "<";
-    DIR_PATHS[{L, D}] = ">";
-    DIR_PATHS[{D, R}] = ">";
-    DIR_PATHS[{R, D}] = "<";
+    DIR_PATHS[{D, L}] = {"<"};
+    DIR_PATHS[{L, D}] = {">"};
+    DIR_PATHS[{D, R}] = {">"};
+    DIR_PATHS[{R, D}] = {"<"};
 
-    DIR_PATHS[{L, R}] = ">>";
-    DIR_PATHS[{R, L}] = "<<";
+    DIR_PATHS[{L, R}] = {">>"};
+    DIR_PATHS[{R, L}] = {"<<"};
 
-    DIR_PATHS[{A,A}] = "";
-    DIR_PATHS[{U,U}] = "";
-    DIR_PATHS[{D,D}] = "";
-    DIR_PATHS[{R,R}] = "";
-    DIR_PATHS[{L,L}] = "";
+    DIR_PATHS[{A,A}] = {""};
+    DIR_PATHS[{U,U}] = {""};
+    DIR_PATHS[{D,D}] = {""};
+    DIR_PATHS[{R,R}] = {""};
+    DIR_PATHS[{L,L}] = {""};
 
 
 
-    std::unordered_map<std::pair<char, char>, std::string, hash_tuple> KEYPAD_PATHS;
+    std::unordered_map<std::pair<char, char>, std::string, hash_tuple> KEYPAD_PATHS1;
     for (int i = 1; i<=9;i++) {
         const auto r1 = (i-1)/3;
         const auto c1 = (i-1)%3;
@@ -232,19 +256,21 @@ int main(const int argc, char *argv[]) {
                 }
             }
 
-            if (c1 < c2) {
-                for (int k = 0; k<(c2-c1); k++) {
-                    tmp += ">";
-                }
-            }
-
             if (r1 < r2) {
                 for (int k = 0; k<(r2-r1); k++) {
                     tmp += "^";
                 }
             }
 
-            KEYPAD_PATHS[{itoc(i),itoc(j)}] = std::move(tmp);
+            if (c1 < c2) {
+                for (int k = 0; k<(c2-c1); k++) {
+                    tmp += ">";
+                }
+            }
+
+
+
+            KEYPAD_PATHS1[{itoc(i),itoc(j)}] = std::move(tmp);
         }
         {
             std::string tmp1;
@@ -273,8 +299,8 @@ int main(const int argc, char *argv[]) {
                 }
             }
 
-            KEYPAD_PATHS[{itoc(i), '0'}] = std::move(tmp1);
-            KEYPAD_PATHS[{'0', itoc(i)}] = std::move(tmp2);
+            KEYPAD_PATHS1[{itoc(i), '0'}] = std::move(tmp1);
+            KEYPAD_PATHS1[{'0', itoc(i)}] = std::move(tmp2);
         }
 
 
@@ -314,106 +340,56 @@ int main(const int argc, char *argv[]) {
                 }
             }
 
-            KEYPAD_PATHS[{itoc(i), A}] = std::move(tmp1);
-            KEYPAD_PATHS[{A, itoc(i)}] = std::move(tmp2);
+            KEYPAD_PATHS1[{itoc(i), A}] = std::move(tmp1);
+            KEYPAD_PATHS1[{A, itoc(i)}] = std::move(tmp2);
         }
     }
 
-    KEYPAD_PATHS[{A, '0'}] = "<";
-    KEYPAD_PATHS[{'0', A}] = ">";
+    KEYPAD_PATHS1[{A, '0'}] = "<";
+    KEYPAD_PATHS1[{'0', A}] = ">";
 
-    std::unordered_map<char, int> t = {{'<', 3}, {'v', 2}, {'>',1}, {'^', 1}};
 
-    for (const auto&[k, s]: KEYPAD_PATHS) {
-        std::cout << k << s << std::endl;
-        simkp(KEYPAD_PATHS[{A, k.first}]+"A"+s+"A");
-        if (s.size()>1) {
-            for (size_t i = 0; i<(s.size()-1); i++) {
-                if (t[s[i]] < t[s[i+1]]) {
-                    std::cout << "invalid" << std::endl;
-                }
-            }
-        }
-    }
-    std::cout << "==============" << std::endl;
-    for (const auto&[k, s]: DIR_PATHS) {
-        std::cout << k << s << std::endl;
-        simdir(DIR_PATHS[{A, k.first}]+"A"+s+"A");
-        if (s.size()>1) {
-            for (size_t i = 0; i<(s.size()-1); i++) {
-                if (t[s[i]] < t[s[i+1]]) {
-                    std::cout << "invalid" << std::endl;
-                }
-            }
-        }
-    }
+    std::unordered_map<std::tuple<char, char, int>, dt, hash_tuple> cache;
+
 
     const auto inp = getInput(argv[1]);
     dt sum = 0;
     for (const auto& kp: inp) {
-        // break;
+
         std::cout << kp  << std::endl;
         dt n = std::stoi(kp);
+
         char curpos = A;
-        std::string seq, seq2, seq3;
+        std::string seq;
+        dt tmp = 0;
+        std::string seq1;
         for (const auto& c: kp) {
-            if (!KEYPAD_PATHS.contains({curpos, c})) {
-                std::cout << "kp"<<curpos<<c<<std::endl;
-            }
-            seq += KEYPAD_PATHS[{curpos, c}] + 'A';
+            seq1 += KEYPAD_PATHS1[{curpos, c}] + 'A';
             curpos = c;
         }
-        simkp(seq);
-        std::cout << seq << std::endl;
-        // simkp(seq);
-        // std::cout << "<A^A>^^AvvvA\n" << std::endl;
 
         curpos = A;
-        for (const auto& c: seq) {
-            if (!DIR_PATHS.contains({curpos, c})) {
-                std::cout << "dp1"<<curpos<<c<<std::endl;
-            }
-
-            // if (tmp.size()==2) {
-            //     if (seq2[seq2.size()-2] == tmp[1][0]) {
-            //         pick = tmp[1];
-            //     }
-            // }
-            seq2 += DIR_PATHS[{curpos, c}] + 'A';
-            curpos = c;
+        for (const auto& c1: seq1) {
+            tmp += dochunk(c1, 1, DIR_PATHS, cache, curpos);
+            curpos = c1;
         }
-        simdir(seq2);
-        std::cout << seq2 << std::endl;
 
-        // std::cout << "v<<A>>^A<A>AvA<^AA>A<vAAA>^A\n" << std::endl;
 
-        curpos = A;
-        for (const auto& c: seq2) {
-            if (!DIR_PATHS.contains({curpos, c})) {
-                std::cout << "dp2"<<curpos<<c<<std::endl;
-            }
-            seq3 += DIR_PATHS[{curpos, c}] + 'A';
-            curpos = c;
-        }
-        simdir(seq3);
-        std::cout << seq3 << std::endl;
-
-        // std::cout << "<vA<AA>>^AvAA<^A>A<v<A>>^AvA^A<vA>^A<v<A>^A>AAvA^A<v<A>A>^AAAvA<^A>A\n"  << std::endl;
-        std::cout << seq3.size() << '*' << n << '=' << seq3.size()*n << std::endl;
-        sum += static_cast<dt>(seq3.size())*n;
+        std::cout << tmp << '*' << n << '=' << tmp*n << std::endl;
+        sum += tmp*n;
     }
-    // lt 289060
-    // gt 270372
-    // neq 275428
-    // neq 282520
-    // neq 276912
-    // new 285840
+    // gt 291239350580826
+    // lt 719206954115056
+    // gt 337244995283638
+    // neq 838939274742794
+    // neq 384041985863254
+    // neq 368151407930732
     std::cout << sum << std::endl;
-    std::cout << (sum < 289060) << std::endl;
-    std::cout << (sum > 270372) << std::endl;
-    std::cout << (sum != 275428) << std::endl;
-    std::cout << (sum != 282520) << std::endl;
-    std::cout << (sum != 276912) << std::endl;
-    std::cout << (sum != 285840) << std::endl;
+    std::cout << (sum > 291239350580826) << std::endl;
+    std::cout << (sum > 337244995283638) << std::endl;
+    std::cout << (sum < 719206954115056) << std::endl;
+    std::cout << (sum != 838939274742794) << std::endl;
+    std::cout << (sum != 384041985863254) << std::endl;
+    std::cout << (sum != 368151407930732) << std::endl;
 }
 
